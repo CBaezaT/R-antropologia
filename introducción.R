0fabb79b1f73cea479188c96f8ef8dc6a3f3a409
Cristian Baeza <crisbaezatorres@gmail.com>
2022-08-12 15:52:00 -0400

    a

--- introducción.R
+++ introducción.R
@@ -1,3 +1,6 @@
 # Bienvenidos al electivo Introducción al análisis de datos en R para Antropología
 # Profesor Cristian Baeza Torres
 # Segundo semestre, cuarto año mención Física 
+
+# Esto es un comentario
+
