d0b25ef3fcda3dabbb842a4ff535993cb725bdc5
Cristian Baeza <crisbaezatorres@gmail.com>
2022-08-12 16:19:37 -0400

    Update introducción.R

--- introducción.R
+++ introducción.R
@@ -4,3 +4,46 @@
 
 # Esto es un comentario
 
+
+# Cuando hablamos de un objeto, es cualquier cosa que podamos manipular en R
+hola <- "Hola Mundo" # creamos un objeto llamado Hola que contiene el texto "Hola Mundo"
+hola 
+
+# En R existen diversos tipos de datos que podemos usar 
+
+1   # numeros enteros // integer
+1.3 # numerico // numeric o float
+"uno" # cadena de texto // character
+uno # factor
+TRUE # lógico // logical boolean
+NA # perdido // NA
+NULL # vacio // null
+
+2022-09-13 # fecha // date
+
+# podemos saber que tipo es con el comando class()
+
+class(1)
+class(1.3)
+class("uno")
+
+
+# R nos permite hacer calculos aritméticos
+ 4 + 3 # suma
+ 4 - 3 # resta
+ 4 * 3 # multiplicación
+ 4 / 3 # división
+ 4^3 # potencia 
+ 4 %% 3 # división entera
+
+# Operadores relacionales
+ 4 < 3 # menor que
+ 4 <= 3 # menor o igual 
+ 4 > 3 # mayor que
+ 4 >= 3 # mayor o igual
+ 4 == 3 # igual
+ != # no es igual, distinto de
+   
+ 
+ 
+
