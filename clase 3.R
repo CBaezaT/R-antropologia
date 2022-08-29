583f9b7397764a0e8c94097172038b20a77ac91e
Cristian Baeza <crisbaezatorres@gmail.com>
2022-08-29 15:18:39 -0400

    clase 3

--- clase 3.R
+++ clase 3.R
@@ -54,5 +54,199 @@ galton <- mutate(galton, midparentHeight = round(midparentHeight*2.54, digits = 
 #childHeight
 galton <- mutate(galton, childHeight = round(childHeight*2.54, digits = 2))
 
+# vemos como nos quedo...
+view(galton)
+
+
+# Ahora... ¿qué podemos sacar de esta información? 
+# ¿Qué se les ocurre al ver las variables?
+resumen_galton <- summary(galton) # guardamos como objeto
+resumen_galton
+
+# ahora, si queremos saber como se dispersan los datos de la media..
+
+# calculamos desviación estandar 
+sd(galton$father)
+
+
+# Queremos crear una tabla para despues ponerla en un informe
+resumen_fathers<-summary(galton$father) 
+resumen_fathers
+
+# estas serian los nombres
+names(resumen_fathers)
+# estas serian las medidas
+as.numeric(resumen_fathers)
+# ahora los unimos en un dataframe usando rbind para que cada valor este en una columna
+tab_1 <- as.data.frame(rbind( as.numeric(resumen_fathers)))
+colnames(tab_1) <- names(resumen_fathers) # cambiamos los colnames a los estadisticos
+tab_1 # vemos como quedo
+write_csv(tab_1, file = "estadisticos galton.csv") # y lo podemos guardar en csv..
+
+# veremos la distribución de las alturas de manera gráfica...
+
+a
+g <- galton
+# R base nos permite realizar gráficos, en este caso usaremos histogramas para ver la distribución y su  media
+# padres
+hist(g$father, main = "Padres", breaks = 30)
+abline(v=mean(g$father), col=2, lwd=2)
+
+hist(g$childHeight[g$sex=="male"], main = "Hijos", breaks = 30)
+abline(v=mean(g$childHeight[g$sex=="male"]), col=2, lwd=2)
+
+hist(c(g$childHeight[g$sex=="male"],g$father), main = "Padres e Hijos", breaks = 30)
+abline(v=mean(c(g$childHeight[g$sex=="male"],g$father)), col=2, lwd=2)
+
+
+
+# otros más... ####
+# madres
+hist(g$mother, main = "Madres", breaks = 30)
+abline(v=mean(g$mother), col=2, lwd=2)
+
+hist(g$childHeight[g$sex=="female"], main = "Hijas", breaks = 30)
+abline(v=mean(g$childHeight[g$sex=="female"]), col=2,lwd=2)
+
+
+
+
+hist(c(g$childHeight[g$sex=="female"],g$mother), main = "Madres e hijas", breaks = 30)
+abline(v=mean(c(g$childHeight[g$sex=="female"],g$mother)), col=2, lwd=2)
+
+#####
+
+
+# Gráficos de barra
+
+plot(g$sex, main = "Gráfico de barras sexo",
+     xlab = "Sexo", ylab = "Frecuencia")
+
+
+# todo el dataset
+# configuramos la salida de graficos para tener 4 al mismo tiempo
+par(mfrow=c(2,2)) # 2 graficos hacia abajo y dos hacia la izquierda
+hist(g$father)
+hist(g$mother)
+hist(g$midparentHeight)
+hist(g$childHeight)
+
+
+# grafico de caja y bigote (boxplot)
+par(mfrow=c(1,1)) # volvemos a dejar 1 solo gráfico de salida
+# padres
+boxplot(g$father,  main = "Gráfico de cajas Padres",
+        xlab = "Padres", ylab = "Altura")
+
+# distribuciones de hijos
+
+boxplot(g$childHeight ~ g$sex,
+        main = "Distribución hijos por sexo",
+        xlab = "Sexo", ylab = "Altura")
+
+# madres
+# ??
+
+
+
+# Ahora de todo el dataset...
+
+boxplot( g$father,  g$mother,
+         names = c("Padres", "Madres"),
+         main = "Boxplot Alturas Padres y Madres",
+         ylab= "Altura")
+
+
+# Gráficos basicos...
+# usaremos el dataset US Murders
+library(dslabs)
+data(murders)
+summary(murders)
+view(murders)
+## Scatterplot
+# Relación entre población y total de asesinatos
+x <- murders$population / 10^6 # por 1 millón
+y <- murders$total
+plot(x, y)
+
+# tambien puede ser 
+with(murders, plot(population, total))
+
+# e incluso...
+
+plot(murders$population, murders$total)
+
+# ¿Cómo lo pondrias más bonito?
+
+
+# Histogramas
+# muy utiles para describir la distribución de los datos númericos..
+x <- with(murders, total / population * 100000)
+hist(x)
+# o tambien...
+
+hist( murders$total/murders$population * 100000, 
+      main = " Tasa de Asesinatos en EEUU ", # es el titulo, por cada 100000 habitantes
+      xlab = "")  # xlab es la etiqueta del eje X, "" lo deja vacio 
+
+# vemos que sobresale
+murders$state[which.max(x)]
+
+
+#  boxplot
+# tambien nos ayudan a ver distribuciones y comparaciones entre categorias..
+
+# creamos la variable rate (tasa de asesinatos cada 100000 habitantes)
+murders$rate <- with(murders, total / population * 100000) # rate es tasa
+
+boxplot(rate~region, data = murders) # tasa por región de estados
+
+# tambien podria ser...
+boxplot(murders$rate~murders$region,
+        xlab = "región",
+        ylab = "Tasa",
+        main = "Asesinatos EEUU")
+# vemos que en el sur hay dos casos extremos, conocidos como outliers
+
+
+
+
+# Ahora.. actividad!
+
+summary(murders)
+class(murders)
+murders<- tibble(murders)
+# crea la variable population_in_millions que contenga la población en millones
+
+
+  # resultado #####
+
+
+population_in_millions <- murders$population/10^6
+murders$population_in_millions <-  murders$population/10^6
+murders <- murders %>% mutate(population_in_millions = population/10^6)
+
+
+# Crea un histograma con la variable population_in_millions #####
+
+  # resultado #####
+
+x <- with(murders,  population / 1000000)
+x <- population_in_millions
+hist(x)
+
+hist(population_in_millions)
+
+hist(murders$population_in_millions)
+
+hist(with(murders, population_in_millions))
+
+with(murders, hist(population_in_millions))
+
+
+# Recordar, hay varias formas de obtener un mismo resultado en R #####
+
+
+# Ahora usando dplyr filtra los estados que tengan población menor a 5 millones
 
 
